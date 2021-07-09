c6762cfd41836a5e08c3f58781b3aff46b8ab667
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2021-07-09 15:27:46 +0900

    fix style and add two agent for human player

--- examples/human_player.py
+++ examples/human_player.py
@@ -3,7 +3,9 @@ import marlgrid
 
 from marlgrid.rendering import InteractivePlayerWindow
 from marlgrid.agents import GridAgentInterface
-from marlgrid.envs import env_from_config
+from marlgrid.envs import env_from_config, registered_envs
+import gym
+
 
 class HumanPlayer:
     def __init__(self):
@@ -23,7 +25,7 @@ class HumanPlayer:
     def start_episode(self):
         self.cumulative_reward = 0
         self.step_count = 0
-    
+
     def end_episode(self):
         print(
             f"Finished episode {self.episode_count} after {self.step_count} steps."
@@ -32,17 +34,17 @@ class HumanPlayer:
         self.episode_count += 1
 
 
-env_config =  {
-    "env_class": "ClutteredGoalCycleEnv",
-    "grid_size": 13,
+env_config = {
+    "env_class": "DoorKeyEnv",
+    "grid_size": 31,
     "max_steps": 250,
-    "clutter_density": 0.15,
-    "respawn": True,
-    "ghost_mode": True,
-    "reward_decay": False,
-    "n_bonus_tiles": 3,
-    "initial_reward": True,
-    "penalty": -1.5
+    # "clutter_density": 0.15,
+    # "respawn": True,
+    # "ghost_mode": True,
+    # "reward_decay": False,
+    # "n_bonus_tiles": 3,
+    # "initial_reward": True,
+    # "penalty": -1.5
 }
 
 player_interface_config = {
@@ -55,35 +57,36 @@ player_interface_config = {
 }
 
 # Add the player/agent config to the environment config (as expected by "env_from_config" below)
-env_config['agents'] = [player_interface_config]
+env_config['agents'] = [player_interface_config, player_interface_config]
 
-# Create the environment based on the combined env/player config
-env = env_from_config(env_config)
+if __name__ == '__main__':
 
-# Create a human player interface per the class defined above
-human = HumanPlayer()
+    # Create the environment based on the combined env/player config
+    env = env_from_config(env_config)
+    # env = gym.make('MarlGrid-2AgentPass30x30-v0')
+    # Create a human player interface per the class defined above
+    human = HumanPlayer()
 
-# Start an episode!
-# Each observation from the environment contains a list of observaitons for each agent.
-# In this case there's only one agent so the list will be of length one.
-obs_list = env.reset()
+    # Start an episode!
+    # Each observation from the environment contains a list of observations for each agent.
+    # In this case there's only one agent so the list will be of length one.
+    obs_list = env.reset()
 
-human.start_episode()
-done = False
-while not done:
+    human.start_episode()
+    done = False
+    while not done:
+        env.render()  # OPTIONAL: render the whole scene + birds eye view
 
-    env.render() # OPTIONAL: render the whole scene + birds eye view
-    
-    player_action = human.action_step(obs_list[0]['pov'])
-    # The environment expects a list of actions, so put the player action into a list
-    agent_actions = [player_action]
+        player_action = human.action_step(obs_list[0]['pov'])
+        # The environment expects a list of actions, so put the player action into a list
+        agent_actions = [player_action, player_action]
 
-    next_obs_list, rew_list, done, _ = env.step(agent_actions)
-    
-    human.save_step(
-        obs_list[0], player_action, rew_list[0], done
-    )
+        next_obs_list, rew_list, done, _ = env.step(agent_actions)
 
-    obs_list = next_obs_list
+        human.save_step(
+            obs_list[0], player_action, rew_list[0], done
+        )
 
-human.end_episode()
+        obs_list = next_obs_list
+
+    human.end_episode()
