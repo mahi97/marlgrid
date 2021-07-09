a9c2c82f50d00bbc561dd3eb24e6e5d5a59d4837
Mohammad Mahdi Rahimi <mohammadmahdi76@gmail.com>
2021-07-09 15:30:33 +0900

    add switch for toggling the door

--- marlgrid/envs/doorkey.py
+++ marlgrid/envs/doorkey.py
@@ -1,7 +1,6 @@
 from ..base import MultiGridEnv, MultiGrid
 from ..objects import *
 
-
 class DoorKeyEnv(MultiGridEnv):
     """
     Environment with a door and key, sparse reward.
@@ -23,7 +22,8 @@ class DoorKeyEnv(MultiGridEnv):
         self.put_obj(Goal(color="green", reward=1), width - 2, height - 2)
 
         # Create a vertical splitting wall
-        splitIdx = self._rand_int(2, width - 2)
+        # splitIdx = self._rand_int(2, width - 2)
+        splitIdx = width // 2
         self.grid.vert_wall(splitIdx, 0)
 
         # Place the agent at a random position and orientation
@@ -31,11 +31,18 @@ class DoorKeyEnv(MultiGridEnv):
         # self.place_agent(size=(splitIdx, height))
 
         # Place a door in the wall
-        doorIdx = self._rand_int(1, width - 2)
-        self.put_obj(Door(color="yellow", state=Door.states.locked), splitIdx, doorIdx)
+        # doorIdx = self._rand_int(1, width - 2)
+        doorIdx = width // 2
+        d1 = Door(color="yellow", state=Door.states.locked)
+        self.put_obj(d1, splitIdx, doorIdx)
 
         # Place a yellow key on the left side
-        self.place_obj(obj=Key("yellow"), top=(0, 0), size=(splitIdx, height))
+        # self.place_obj(obj=Key("yellow"), top=(0, 0), size=(splitIdx, height))
+        s1 = Switch("yellow")
+        self.place_obj(obj=s1, top=(width // 4, height - 5), size=(3, 3))
+        self.place_obj(obj=Key("yellow"), top=(width // 4, height - 5), size=(3, 3))
 
-        self.agent_spawn_kwargs = {}
-        self.place_agents(**self.agent_spawn_kwargs)
+        self.agent_spawn_kwargs = {'top': (0, 0), 'size': (width // 2, height // 2)}
+
+        s1.signal.connect(d1.switch)
+        # self.place_agents(**self.agent_spawn_kwargs)
